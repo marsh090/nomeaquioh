c29031c87d8d5219c13a3ad6b17d9b75ccd15add
Luchaos, Artificer of Ilha das Flores <89659834+marsh090@users.noreply.github.com>
2023-02-09 10:45:18 -0300

    Update code.c

--- code.c
+++ code.c
@@ -22,3 +22,52 @@ int main()
 
     return 0;
 }
+
+
+
+/* AULA 2
+// programa que recolha o peso de tres boi e fale qual o mais gordo
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    
+    int i;
+    float bois[3] = {};
+    int i_maior;
+    // declarando e inicializando o vetor bois
+  
+    printf("Entre os valores do vetor\n");
+  
+    // loop de inserção dos valores no vetor
+    for(i = 0 ; i <= 2; i++)
+    {
+        printf("Peso boi[%d]: ", i+1);
+        scanf("%f", &bois[i]);
+    }
+    
+    i_maior = 0;
+    
+    for(i = 0 ; i <= 2; i++)
+    {
+        if(bois[i] > bois[i_maior]){
+            i_maior = i;
+        }
+    }
+    
+    // logica para encontrar valor maximo do vetor
+    if(bois[0] > bois[1] && bois[0] > bois[2]){
+            printf("O boi 1 é o mais pesado");
+    }else if(bois[1] > bois[2]){
+        printf("O boi 2 é o mais pesado");
+    }else{
+        printf("O boi 3 é o mais pesado");
+    }
+    
+    
+    return 0;
+}
+*/
